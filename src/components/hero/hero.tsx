c4d7681ed914b1fbe6251e1adcca3a7b77d84c0c
Manas Malla <manasmalla.dev@gmail.com>
2024-07-21 13:35:02 +0530

    added the basic structure of the hero component
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -4,13 +4,21 @@ import styles from "./hero.module.css";
 export default function Hero() {
   return (
     <div className={`${styles.hero} flex flex-col w-full gap-3`}>
-      <div className={`${styles.carousel} rounded-3xl`}></div>
+      <div className={`${styles.carousel} rounded-3xl`}>
+        <img
+          src="/images/products/hair-shampoo-banner.png"
+          className="h-full w-full object-cover rounded-3xl"
+        />
+      </div>
       <div className="grow gap-3 flex flex-col">
         <div className="h-[64%] relative">
           <HeroCard>
             <p>Hello</p>
           </HeroCard>
-          <img src="" className="absolute" />
+          <img
+            src="/images/leaves-right.png"
+            className="absolute -bottom-4 h-[25vh] -right-4"
+          />
         </div>
         <div className="flex gap-3 grow">
           <HeroCard></HeroCard>
