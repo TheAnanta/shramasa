ddc7ec3352f4c6db011d3abbbaf000755d758d8c
Manas Malla <manasmalla.dev@gmail.com>
2024-07-22 22:39:42 +0530

    Delete src/components/hero directory

--- src/components/hero/hero-card.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./hero.module.css";
-
-export default function HeroCard({ className, children }: any) {
-  return (
-    <div className={`${styles.card} w-full rounded-3xl h-full ${className}`}>
-      {children}
-    </div>
-  );
-}
