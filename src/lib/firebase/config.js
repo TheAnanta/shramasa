cc98581d0ab8e859c2e20cf325384ae955ef5b3a
Manas Malla <manasmalla.dev@gmail.com>
2024-07-22 14:43:33 +0530

    added firebase
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

---
+++ src/lib/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCIDM9iDU2AZHOjzbKnlUxiyQO3_plRl5o",
+  authDomain: "shramasa-care.firebaseapp.com",
+  projectId: "shramasa-care",
+  storageBucket: "shramasa-care.appspot.com",
+  messagingSenderId: "755136036470",
+  appId: "1:755136036470:web:e81741063bec1690a6a957",
+  measurementId: "G-8CRPQETYXT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
