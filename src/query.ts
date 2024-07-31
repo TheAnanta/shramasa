2f2f208b88b80cc5cb47678790c59e1cf6910126
Manas Malla <manasmalla.dev@gmail.com>
2024-07-31 15:07:55 +0530

    added update api
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/query.ts
+++ src/query.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 
 app.get("/get-all-categories", async (_: Request, res: Response) => {
   try {
@@ -33,6 +36,29 @@ app.post("/add-category", async (req: Request, res: Response) => {
 
     res.status(200).json(category);
   } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/update-category-by-id", async (req, res)=>{
+  const { name, image, categoryId } = req.body;
+  if(!categoryId){
+    return res.status(400).json({error: "Invalid category id."});
+  }
+  if (!name && !image) {
+    return res.status(400).json({ error: "Field to be updated not provided." });
+  }
+  // return res.send(categoryId + " " + image);
+  try{
+    const updatedCategory = await prisma.category.update({where: {
+      categoryId: categoryId
+    }, data:{
+      name: name,
+      image: image
+    }});
+    res.status(200).json(updatedCategory);
+  }catch(error){
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
   }
