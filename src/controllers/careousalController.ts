6749e925b7acbdfdfefed86c80201ed9cb68a7e4
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-08-15 12:52:43 +0530

    carousal api added
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

---
+++ src/controllers/careousalController.ts
@@ -0,0 +1,17 @@
+import { Response, Request } from "express";
+import prisma from "../prismaClient";
+
+export const getCarousal = async (req: Request, res: Response) => {
+  try {
+    const carousal = await prisma.product.findMany();
+    //sort carousal by latest time,and limit it 5
+    carousal.sort((a, b) => {
+      return b.dateAdded.getTime() - a.dateAdded.getTime();
+    });
+    carousal.slice(0, 5);
+
+    res.status(200).json(carousal);
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+};
