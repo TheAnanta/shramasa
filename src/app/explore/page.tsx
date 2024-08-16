6d5bac32a6e76e26c0e48a2aed37bde12386fb7d
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-08-16 13:27:13 +0530

    explore page skeleton added
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,9 +1,77 @@
-import Explore from "@/components/Explore/Explore"
+"use client";
+import React from "react";
+import Item from "../../components/Item";
+import styles from "./explore.module.css";
+import { Product } from "@/types/interfaces";
 
-export default function Page() {
+export default function Explore() {
+  const [open, setOpen] = React.useState(false);
+  const [products, setProducts] = React.useState<any>();
+
+  React.useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        "http://localhost:3001/api/products/get-all-products"
+      );
+      const data = await response.json();
+      // console.log(data);
+      setProducts(data);
+    }
+    getData();
+  }, []);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div>
-        <Explore />
+    <div className="flex flex-col lg:flex-row items-start justify-between mx-[8.25%] pt-12">
+      <button
+        className="lg:hidden mb-4 px-4 py-2 rounded-full border-[#46A627] border text-black"
+        onClick={toggleDrawer}
+      >
+        {open ? "Hide Filters" : "Show Filters"}
+      </button>
+      <div
+        className={`${styles.drawer} pr-[4.46%] w-full lg:w-[30%] ${
+          open ? "block" : "hidden"
+        } lg:block`}
+      >
+        <div className="flex flex-col justify-between space-y-4 text-xl">
+          <h2 className="text-2xl font-semibold hidden sm:flex">Filters</h2>
+          <div className="flex flex-col space-y-4 lg:space-y-5 items-start justify-center lg:h-[60%] pb-12">
+            <p className="pb-0">Price</p>
+            <img src="/price.svg" alt="" className="pb-2" />
+            <p>
+              <span>+</span> Ingredient
+            </p>
+            <p>+ Category</p>
+            <p>+ Occasion</p>
+            <p>+ Formulation</p>
+            <p>+ Skin Tone</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Items Grid */}
+      {products ? (
+        products.map((product: any, index: number) => {
+          return (
+            <div
+              key={product.name}
+              className="grid grid-cols-2 lg:grid-cols-3 gap-8"
+            >
+              <Item
+                name={product?.name}
+                description={product?.description}
+                price={product?.price}
+              />
+            </div>
+          );
+        })
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
-  )
+  );
 }
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,14 +1,21 @@
+import { Product } from "@/types/interfaces";
 import React from "react";
 
-export default function Item() {
+export default function Item(props: {
+  name: string;
+  description: string;
+  price: string;
+}) {
   return (
     <div className="flex flex-col items-start justify-center gap-y-2">
-      <img src="/newSection/product.svg" alt="" />
-      <p className="pt-4">face cream</p>
-      <p>Lorem ipsum dolor amet sit contest</p>
+      <img src="/images/products/mockup.png" alt="" />
+      <p className="pt-4">{props.name}</p>
+      <p>{props.description}</p>
       <div className="flex space-x-3 items-end">
-        <p className="text-lg font-semibold">₹20.19 </p>
-        <p className="text-[#999999] line-through text-base">₹22.35</p>
+        <p className="text-lg font-semibold">₹{props.price}</p>
+        <p className="text-[#999999] line-through text-base">
+          {props.price + 12}
+        </p>
       </div>
     </div>
   );
