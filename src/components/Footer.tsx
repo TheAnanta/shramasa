ed4993952eb22d09fc5c8584329b20b60ebde28b
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-09-23 10:23:29 +0530

    footer edited
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,13 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="bg-[#F2F2F2] dark:bg-neutral-950 flex flex-col sm:flex-row items-center justify-center sm:items-start sm:justify-between px-[8.25%] py-20">
-      <div className="flex flex-col justify-center items-center sm:items-start md:justify-start pb-10 sm:pb-0">
+      <div className="flex flex-col justify-center items-center sm:items-start md:justify-start pb-10 sm:pb-0  font-semibold">
         <img src="/footer/footer-logo.svg" alt="" className="pb-12" />
         <p className="sm:text-justify text-center text-[#585858] dark:text-white">
-          Lorem ipsum dolor sit amet, justo dolor.
+          Welcome to Shramasa - A one stop solution for all your beauty needs. 
         </p>
         <p className="sm:text-justify text-center text-[#585858] dark:text-white">
-          consectetur adipiscing elit. Donec congue
-        </p>
-        <p className="sm:text-justify text-center text-[#585858] dark:text-white">
-          Donec congue justo dolor.
+          From Hair Care to Skin Care, we have it all.
         </p>
       </div>
       <div className="flex flex-col items-center sm:items-end justify-between h-8 sm:h-[20vh]">
