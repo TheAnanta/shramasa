705d8fbae2311577af1e76ff858a31c649f3fd5d
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-12-01 15:55:44 +0530

    review api added
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/controllers/reviewController.ts
+++ src/controllers/reviewController.ts
@@ -12,3 +12,29 @@ export const getReview: any = async (
     return res.status(500).json({ error: error.message });
   }
 };
+
+export const postReview: any = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const request: any = await req.body;
+    const message = request.message;
+    const userId = request.userId;
+    const rating = request.rating;
+
+    const review = await prisma.review.create({
+      data: {
+        reviewId: String(userId),
+        userId,
+        rating,
+        reviewText: message,
+        dateAdded: String(Date.now()),
+      },
+    });
+
+    return res.json({ review: review, status: 201 });
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
